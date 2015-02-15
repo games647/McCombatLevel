2c6c4e11d84ebb0d79ddbc4e4bd278f3db992f77
games647 <games@tsprivat.de>
2015-02-15 15:57:20 +0100

    Add /level player

--- src/main/java/com/gmail/mrphpfan/LevelCommand.java
+++ src/main/java/com/gmail/mrphpfan/LevelCommand.java
@@ -1,5 +1,6 @@
 package com.gmail.mrphpfan;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,16 +17,34 @@ public class LevelCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("You must be a player to execute this command.");
+        if (args.length >= 1) {
+            return onLevelOther(sender, args[0]);
+        }
+
+        return onLevelSelf(sender);
+    }
+
+    private boolean onLevelOther(CommandSender sender, String target) {
+        Player targetPlayer = Bukkit.getPlayer(target);
+        if (targetPlayer == null) {
+            sender.sendMessage(ChatColor.DARK_RED + "Player: '" + target + "' not found");
             return true;
         }
 
-        if (command.getName().equalsIgnoreCase("combatlevel")) {
-            int level = pluginInstance.getCombatLevel((Player) sender);
-            sender.sendMessage(ChatColor.GOLD + "Combat level: " + ChatColor.DARK_GREEN + level);
+        int level = pluginInstance.getCombatLevel(targetPlayer);
+        sender.sendMessage(ChatColor.GOLD + targetPlayer.getName() + "'s Combat level: " + ChatColor.DARK_GREEN + level);
+        return true;
+    }
+
+    private boolean onLevelSelf(CommandSender sender) {
+        if (!(sender instanceof Player)) {
+            //non player cannot have a combat level
+            sender.sendMessage(ChatColor.DARK_RED + "You must be a player to execute this command");
+            return true;
         }
 
+        int level = pluginInstance.getCombatLevel((Player) sender);
+        sender.sendMessage(ChatColor.GOLD + "Combat level: " + ChatColor.DARK_GREEN + level);
         return true;
     }
 }
